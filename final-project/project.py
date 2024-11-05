693bf629eaf1671823cdf8cbc1a01776c4098b79
Alex Harris <nharris@pdx.edu>
2024-11-05 11:33:40 -0800

    Giving myself some notes.

--- final-project/project.py
+++ final-project/project.py
@@ -5,6 +5,16 @@ import matplotlib
 from scipy import fft
 
 
+# This project is going to be a tuner with several options.
+# First is automatic detection.
+
+# Second is to generate a standing wave of specific frequencies
+
+# Third is going to be interactive to select which note and measure what was
+# received and give a good or negative response indicating if it was close enough
+# or way off. Floating point value +/- of how far off it was?
+
+
 if __name__ == "__main__":
     print("I am main!")
     exit()

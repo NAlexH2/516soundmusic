051fcf02e28518051ecd7f114495bc805cc9e8cf
NAlexH2 <alex@nah-red.net>
2024-12-02 17:01:37 -0800

    Broken but going forward with it.

--- final-project/resources/automatic.py
+++ final-project/resources/automatic.py
@@ -1,13 +1,117 @@
 import os
+import pyaudio
+from pyaudio import PyAudio, Stream, get_format_from_width
 import numpy as np
 import sounddevice as sd
-import librosa
-from scipy import fft
-from .projGlobals import *
+from projGlobals import *
 
 
 class AutomaticDetection:
 
+    def __init__(self):
+        self.pitch = 0.0
+        self.notes = list(NOTES_DICT.values())
+        self.nearest_note = ""
+        self.nearest_pitch = ""
+        self.win_samples = np.zeros(SAMPLE_RATE, dtype=np.float32)
+        self.rec_dev_num = None
+        self.rec_dev_name = None
+
+    def selectRecordingDevice(self):
+        termClear()
+        device_list = dict()
+        p = PyAudio()
+        info = p.get_host_api_info_by_index(host_api_index=0)
+        for i in range(info.get("deviceCount")):
+            if p.get_device_info_by_host_api_device_index(0, i).get(
+                "maxInputChannels"
+            ):
+                devInfo = p.get_device_info_by_host_api_device_index(0, i)
+                iInfo = devInfo.get("index")
+                nInfo = devInfo.get("name")
+                device_list[iInfo] = nInfo
+
+        opt = -1
+        conf = "n"
+        while opt < 1 or opt > len(device_list) or conf != "Y".lower():
+            print(
+                "Select which device to use for live audio processing "
+                "and note comparison: "
+            )
+            try:
+                for d in range(len(device_list)):
+                    print(f"   #{d+1} - Name: {device_list[d]}")
+                opt = int(input("\nPlease enter which device number to use: "))
+            except ValueError:
+                opt = -1
+                termClear()
+                print("Invalid option...\n")
+                continue
+            if opt > len(device_list) or opt < 1:
+                opt = -1
+                termClear()
+                print("Invalid option...\n")
+                continue
+            else:
+                print(f"Selected device --> {device_list[opt-1]}.")
+                conf = input("Is this correct? Y/N: ").lower()
+                if conf != "y":
+                    conf = "n"
+                    termClear()
+                    continue
+
+        self.rec_dev_num = opt - 1
+        self.rec_dev_name = device_list[opt - 1]
+        return
+
+    def nearest_neighbor(self):
+        notes_len = len(self.notes)
+        n = np.round((self.pitch / PITCH_CHECK) * notes_len)
+        self.nearest_note = self.notes[n % notes_len]
+        self.nearest_pitch = PITCH_CHECK * 2 ** (n / notes_len)
+        return
+
+    def callback(self, inputD: np.ndarray, frames, dur, state):
+        if any(inputD):
+            self.win_samples = np.concatenate((self.win_samples, inputD[:, 0]))
+            self.win_samples = self.win_samples[inputD[:, 0].size :]
+
+            fft_res = np.fft.fft(self.win_samples)
+            magnitude = np.abs(fft_res)
+            self.pitch = np.fft.fftfreq(fft_res.size, 1 / SAMPLE_RATE)
+            self.nearest_neighbor()
+            termClear()
+            expt_freq = float(
+                "{:.3f}".format(NOTES_TO_FREQ_DICT[self.nearest_note])
+            )
+            freq_diff = float("{:.3f}".format(expt_freq - self.pitch))
+            print(
+                f"Nearest note: {self.nearest_note}\n"
+                f"Expected freq: {expt_freq}\n"
+                f"Actual freq: {self.pitch}\n"
+                f"{freqDifference(self.nearest_note, freq_diff)}"
+            )
+        else:
+            print("Waiting for you to play!")
+        print("CTRL+C to return to main menu!")
+
     def autoStart(self):
         termClear()
-        print("auto start")
+        self.selectRecordingDevice()
+        try:
+            with sd.InputStream(
+                samplerate=SAMPLE_RATE,
+                blocksize=WINDOW_SLIDE,
+                device=self.rec_dev_num,
+                channels=1,
+                callback=self.callback,
+            ):
+                while True:
+                    pass
+        except KeyboardInterrupt:
+            return
+
+
+if __name__ == "__main__":
+    ad = AutomaticDetection()
+    ad.autoStart()
--- notebook.md
+++ notebook.md
@@ -1,6 +1,7 @@
 # Table of Contents
 - [Table of Contents](#table-of-contents)
 - [Note to reader](#note-to-reader)
+- [12/2/24 - 2:00 PM - Wrapping the project up today... I hope...](#12224---200-pm---wrapping-the-project-up-today-i-hope)
 - [12/1/24 - 12:48 PM - About working today...](#12124---1248-pm---about-working-today)
 - [11/30/24 - 9:32 AM - Working on Final Project](#113024---932-am---working-on-final-project)
 - [11/26/24 - 2:20PM -\> 6:00PM - Setup and Work On Final Portfolio Objective](#112624---220pm---600pm---setup-and-work-on-final-portfolio-objective)
@@ -30,6 +31,25 @@ oldest is at the bottom.
 
 Table of contents are generated using a VSCode extension called 
 "Markdown All In One." Very useful tool.
+
+# 12/2/24 - 2:00 PM - Wrapping the project up today... I hope...
+> We'll see...
+- Found an article that can help move me forward on writing the automatic 
+detection, but it focuses more on a large range of notes, but I'm just focussing
+on making a tuner specifically for guitars.
+- This involved re-writing a few of the variables to better suit this stage of 
+the project.
+  - Specifically, what to display with the scaling approximation.
+  - You'll see in [projGlobals.py](final-project/resources/projGlobals.py) that 
+  it went to specific notes (E2 -> E4) instead of stating "E (low) -> E (high)"
+- This is proving to be more difficult than I originally thought...
+  - Having issues with live parsing the information because it's inaccurate 
+  math (again...)
+  - The input is calculated way too high.
+  - The nearest note is very wrong (E2 == G3 for some reason????)
+  - When adjusting code, it seemingly falls apart.
+  - I'm going to commit the broken piece and work on it because I know that's 
+  the route I need take.
 
 # 12/1/24 - 12:48 PM - About working today...
 - I decided I did enough yesterday. The final step can be saved for later this
